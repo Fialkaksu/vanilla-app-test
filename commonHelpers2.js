4ebc493fea82a45a8a69396f403ec05fd0cfd3a6
Fialkaksu <46996947+Fialkaksu@users.noreply.github.com>
2024-05-28 12:40:32 +0000

    Deploying to gh-pages from @ Fialkaksu/vanilla-app-test@aaf7549c551285fd9a3a8a6e7a37efcae55f4ded 🚀

--- commonHelpers2.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as o,t as n}from"./assets/vendor-1e54b975.js";const c=document.querySelector("iframe#vimeo-player"),t=new o(c),r="videoplayer-current-time",a=JSON.parse(localStorage.getItem(r))||0;t.setCurrentTime(a).catch(function(e){console.error("Error setting current time:",e)});const i=n(function(e){localStorage.setItem(r,e.seconds)},1e3);t.on("timeupdate",i);
-//# sourceMappingURL=commonHelpers2.js.map
